78892763d667fa9020b0558eb5a43553d357c5c2
LordSir101 <tywunderlich15@gmail.com>
2020-11-15 21:45:43 -0500

    Added basic seach function

--- src/django/app/views.py
+++ src/django/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import http.client
+import json
 
 
 # Create your views here.
@@ -9,3 +11,39 @@ def home (request):
 
 def favourites (request):
     return render(request, 'app/favourites.html')
+
+def searchName(request):
+    args = {}
+    if request.method == "POST":
+        req = request.POST.get('searchBar')
+
+        conn = http.client.HTTPSConnection("apidojo-yahoo-finance-v1.p.rapidapi.com")
+
+        headers = {
+            'x-rapidapi-key': "d9ef4f5324msh8ace8b2abea3cc2p18ac4ajsn03ca9ecf4926",
+            'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+            }
+
+        conn.request("GET", "/auto-complete?q="+req+"&region=US", headers=headers)
+
+        res = conn.getresponse()
+        resBody= res.read()
+        data = json.loads(resBody)
+
+        i = 0
+        #data.decode("utf-8")
+        for exchange in data['quotes']:
+            args[i] = {}
+            args[i]["exchange"] = exchange['exchange']
+            try:
+                args[i]["name"] = exchange['longname']
+            except:
+                try:
+                    args[i]["name"] = exchange['shortname']
+                except:
+                    args[i]["name"] = "Name Unavailable"
+            args[i]["symbol"] = exchange['symbol']
+            args[i]["type"] = exchange['quoteType']
+            i += 1
+        print(args)
+    return render(request, 'app/searchForm.html', {'args':args})
