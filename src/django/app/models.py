70df05a3dd2dd575d398a2e2e69678e69f0db639
LordSir101 <tywunderlich15@gmail.com>
2020-11-21 11:46:06 -0500

    modified comment query to work with models

--- src/django/app/models.py
+++ src/django/app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Stock(models.Model):
+    name = models.CharField(max_length=5)
+    ticker= models.CharField(max_length=10)
+    def __str__(self):
+        return "%s" % self.ticker
+class Trader (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorites = models.ManyToManyField(Stock)
+    def __str__(self):
+        return "%s" % self.user
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    posted_on = models.DateField()
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    about = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s on %s" % (self.posted_by, self.about)
--- src/django/app/views.py
+++ src/django/app/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
 from datetime import datetime, timedelta
-
-
+from app.models import Comment, Stock, User, Trader
 import http.client
 import json #to parse finance API
-from django.db import connection #access to default database in monte_project/settings.py
-
-
 
 # Create your views here.
 
@@ -29,7 +25,7 @@ def simulate (request, stockSymbol):
 
     conn.request("GET", f'/stock/v2/get-statistics?symbol={stockSymbol}&region=US', headers=headers)
     res = conn.getresponse()
-    
+
     try:
         data = json.loads(res.read().decode("utf-8"))
     except:
@@ -40,21 +36,23 @@ def simulate (request, stockSymbol):
     stockInfo['symbol'] = stockSymbol
     stockInfo['name'] = data["price"]["longName"]
     stockInfo['change'] = round(data["price"]["regularMarketChangePercent"]["raw"] * 100, 2)
-  
-
 
     #find all comments for the stock that was clicked on
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM comments WHERE Ticker = (%s)', (stockInfo['symbol'],))
-    comments = cursor.fetchall()
+
+    #c = User.objects.filter(commentaboutname=l)
+    comments = Comment.objects.filter(about__ticker=stockSymbol)
+
+    #cursor = connection.cursor()
+    #cursor.execute('SELECT * FROM comments WHERE Ticker = (%s)', (stockInfo['symbol'],))
+    #comments = cursor.fetchall()
 
     #gather all information about each comment
     i = 0
     for comment in comments:
         commentInfo[i] = {}
-        commentInfo[i]["user"] = comment[0]
-        commentInfo[i]["date"] = comment[2]
-        commentInfo[i]["content"] = comment[3]
+        commentInfo[i]["user"] = str(comment.posted_by)
+        commentInfo[i]["date"] = str(comment.posted_on)
+        commentInfo[i]["content"] = comment.text
         i += 1
     #print(comments)
     dateInfo = {}
@@ -102,5 +100,5 @@ def searchName(request):
                 args[i]["symbol"] = exchange['symbol']
                 args[i]["type"] = exchange['quoteType']
                 i += 1
-    
+
     return render(request, 'app/searchForm.html', {'args':args})
