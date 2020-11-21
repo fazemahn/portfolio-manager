862547f5a36e6858f8c7f5e4acd95c4ffe551856
Graem Sheppard <graemsheppard@gmail.com>
2020-11-21 13:54:48 -0500

    increased length of some model fields

--- src/django/app/models.py
+++ src/django/app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Stock(models.Model):
-    name = models.CharField(max_length=5)
+    name = models.CharField(max_length=75)
     ticker= models.CharField(max_length=10)
     def __str__(self):
         return "%s" % self.ticker
@@ -14,7 +14,7 @@ class Trader (models.Model):
     def __str__(self):
         return "%s" % self.user
 class Comment(models.Model):
-    text = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
     posted_on = models.DateField()
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     about = models.ForeignKey(Stock, on_delete=models.CASCADE)
