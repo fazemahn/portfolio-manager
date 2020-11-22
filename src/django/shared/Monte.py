f0ca6c199a77090f8aa28429d430a144908f180c
Scott Garland <scott.garland@ontariotechu.net>
2020-11-22 18:30:42 -0500

    Update Monte.py

--- src/django/shared/Monte.py
+++ src/django/shared/Monte.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 import mpld3
 from scipy.stats import norm
 
