594196042eb4dc24a29d3c5d16258c1abc0def5f
Scott Garland <scott.garland@ontariotechu.net>
2020-11-22 16:13:47 -0500

    taking out unneccessary library imports

--- src/django/app/api.py
+++ src/django/app/api.py
@@ -2,14 +2,8 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.http import HttpResponseBadRequest
 from django.http import Http404
-
-from Monte import Monte
 import datetime as dt
-import numpy as np 
-import pandas as pd
-import pandas_datareader as pdr
-import matplotlib.pyplot as plt
-from scipy.stats import norm
+from shared.Monte import Monte
 
 
 def simulate (request):
--- src/django/shared/monte_driver.py
+++ src/django/shared/monte_driver.py
@@ -1,10 +1,5 @@
 from Monte import Monte
 import datetime as dt
-import numpy as np 
-import pandas as pd
-import pandas_datareader as pdr
-import matplotlib.pyplot as plt
-from scipy.stats import norm
 
 
 """
