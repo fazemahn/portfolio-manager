591e90e2e7cd0d32d0c99bc5089bef9181d7d5cc
Graem Sheppard <graemsheppard@gmail.com>
2020-11-23 11:31:36 -0500

    restored remstock

--- src/django/app/urls.py
+++ src/django/app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('simulate/<str:stockSymbol>/', views.simulate, name = 'app-simulate'),
     path ('search/', views.searchName),
     path('api/simulate', api.simulate, name = 'api-simulate'),
-    path('addfav/<str:stockSymbol>/<str:stockName>', views.addfav, name = 'addfav'),
-    path('favourites/<str:stockSymbol>/removefav', views.remfav, name = 'remfav'),
+    path('favourites/addfav/<str:stockSymbol>/<str:stockName>', views.addfav, name = 'addfav'),
+    path('favourites/remfav/<str:stockSymbol>/', views.remfav, name = 'remfav'),
     path('favourites/<int:commID>/removecomment', views.remcom, name= 'remcom')
 
 ]
